b1d08be562ee544cd8234b3254d2345d841f2f55
LY325495 <3254954760@qq.com>
2025-01-28 20:24:48 +0800

    修复打包构建依赖找不到问题

--- src/pages/Faculty/Faculty.js
+++ src/pages/Faculty/Faculty.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react';
-import {connect} from "../../.umi/plugin-dva/exports";
-import AdvancedSearchForm from "../../conponent/AdvancedSearchForm";
-import {Card, Icon} from "antd";
-import StandardTable from "../../conponent/StandardTable";
-@connect(({FacultyModal,loading})=>({
+import React, { Component } from 'react';
+import { connect } from 'umi';
+import AdvancedSearchForm from '../../conponent/AdvancedSearchForm';
+import { Card, Icon } from 'antd';
+import StandardTable from '../../conponent/StandardTable';
+@connect(({ FacultyModal, loading }) => ({
   FacultyModal,
-  dataLoading:loading.effects['FacultyModal/fetch'],
+  dataLoading: loading.effects['FacultyModal/fetch'],
 }))
 class Faculty extends Component {
   constructor() {
     super();
-    this.state={
-      searchMoreParams:{},
-    }
+    this.state = {
+      searchMoreParams: {},
+    };
   }
   componentDidMount() {
     this.listPage({
@@ -20,7 +20,7 @@ class Faculty extends Component {
       pageSize: 10,
     });
   }
-  listPage = params => {
+  listPage = (params) => {
     const {
       dispatch,
       FacultyModal: { pagination },
@@ -29,93 +29,97 @@ class Faculty extends Component {
     const isParamsUndefined = Object.keys(params || {});
     dispatch({
       type: 'FacultyModal/fetch',
-      payload: isParamsUndefined.length !== 0 ? params : { ...pagination, ...searchMoreParams },
+      payload:
+        isParamsUndefined.length !== 0
+          ? params
+          : { ...pagination, ...searchMoreParams },
     });
   };
 
-  advancedSearch = (searchMoreParams) =>{
-    console.log(searchMoreParams)
-    this.setState({searchMoreParams})
+  advancedSearch = (searchMoreParams) => {
+    console.log(searchMoreParams);
+    this.setState({ searchMoreParams });
     const { dispatch } = this.props;
     dispatch({
       type: 'FacultyModal/fetch',
-      payload: {// 搜索之后总是第一页
+      payload: {
+        // 搜索之后总是第一页
         ...searchMoreParams,
         currentPage: 1,
         pageSize: 10,
       },
     });
-  }
+  };
 
-  handleStandardTableChange =(pagination, filters, sorter)=>{
+  handleStandardTableChange = (pagination, filters, sorter) => {
     // 到时候 获取 pagination  和 searchMore params
-    const {searchMoreParams} = this.state
+    const { searchMoreParams } = this.state;
     const parmas = {
       ...searchMoreParams,
-      currentPage:pagination.current,
-      pageSize:pagination.pageSize
-    }
-    this.listPage(parmas)
-  }
+      currentPage: pagination.current,
+      pageSize: pagination.pageSize,
+    };
+    this.listPage(parmas);
+  };
   render() {
-    const inputOption =[
-      {key:"计算机科学与技术学院",value:"计算机科学与技术学院"},
-      {key:"法学院",value:"法学院"},
-      {key:"理学院",value:"理学院"},
-      {key:"生命学院",value:"生命学院"},
-      {key:"体育学院",value:"体育学院"},
-      {key:"信息学院",value:"信息学院"},
-    ]
-    const searchList=[
+    const inputOption = [
+      { key: '计算机科学与技术学院', value: '计算机科学与技术学院' },
+      { key: '法学院', value: '法学院' },
+      { key: '理学院', value: '理学院' },
+      { key: '生命学院', value: '生命学院' },
+      { key: '体育学院', value: '体育学院' },
+      { key: '信息学院', value: '信息学院' },
+    ];
+    const searchList = [
       {
         title: '院系',
-        field:'faculty',
+        field: 'faculty',
         type: 'select',
-        src:inputOption,
-      }
-    ]
-    const columns=[
+        src: inputOption,
+      },
+    ];
+    const columns = [
       {
-        title:'学号',
-        dataIndex:'studentId',
-        key:'STUDENTID',
+        title: '学号',
+        dataIndex: 'studentId',
+        key: 'STUDENTID',
       },
       {
-        title:'姓名',
-        dataIndex:'name',
-        key:'NAME',
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'NAME',
       },
       {
-        title:'性别',
-        dataIndex:'gender',
-        key:'GENDER',
+        title: '性别',
+        dataIndex: 'gender',
+        key: 'GENDER',
       },
       {
-        title:'院系',
-        dataIndex:'departmentName',
-        key:'departmentName',
+        title: '院系',
+        dataIndex: 'departmentName',
+        key: 'departmentName',
       },
       {
-        title:'宿舍楼号',
-        dataIndex:'buildingName',
-        key:'buildingName',
+        title: '宿舍楼号',
+        dataIndex: 'buildingName',
+        key: 'buildingName',
       },
       {
-        title:'电话号码',
-        dataIndex:'phoneNumber',
-        key:'phoneNumber',
+        title: '电话号码',
+        dataIndex: 'phoneNumber',
+        key: 'phoneNumber',
       },
       {
-        title:'宿舍',
-        dataIndex:'dormitoryNumber',
-        key:'dormitoryNumber',
-        render:(text)=> text===null? "尚未分配":text
+        title: '宿舍',
+        dataIndex: 'dormitoryNumber',
+        key: 'dormitoryNumber',
+        render: (text) => (text === null ? '尚未分配' : text),
       },
-    ]
+    ];
     const {
-      FacultyModal:{studentData,pagination} ,
-      dataLoading
-    } =this.props
+      FacultyModal: { studentData, pagination },
+      dataLoading,
+    } = this.props;
     return (
       <div>
         <AdvancedSearchForm
@@ -128,7 +132,7 @@ class Faculty extends Component {
             columns={columns}
             data={studentData}
             rowSelection={null}
-            rowKey={record => record.studentId}
+            rowKey={(record) => record.studentId}
             loading={dataLoading}
             onChange={this.handleStandardTableChange}
           />
@@ -136,7 +140,6 @@ class Faculty extends Component {
       </div>
     );
   }
-
 }
 
 export default Faculty;
